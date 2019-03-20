b1b1ebd85ec01eacf0f7a30089578b7c31e84baa
dagal <galaktionov@gmail.com>
2019-03-20 09:50:50 +0100

    Testing Gin framework

--- main.go
+++ main.go
@@ -5,10 +5,24 @@ package main
 // #include "cgo.hxx"
 import "C"
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hi from Go, about to calculate distance in C++ ...")
 	distance := C.distance_between(1.0, 1.0, 2.0, 2.0)
 	fmt.Printf("Go has result, distance is: %v\n", distance)
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	if err := r.Run(); err != nil {
+		// pretend I'm handling it...
+	}
 }
