d332147a0aaf2328080980322c240be18c229d9d
dagal <galaktionov@gmail.com>
2019-03-21 12:22:48 +0100

    Init DB

--- main.go
+++ main.go
@@ -12,9 +12,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"os"
+	"os/exec"
 )
 
-func initDB() {
+func readCSV() {
 	file, err := os.Open("data/madrid_emt/stops.txt")
 	if err != nil {
 		log.Fatalln(err)
@@ -27,9 +28,49 @@ func initDB() {
 		log.Fatalln(err)
 	}
 
-	for _, r := range records {
-		fmt.Println(r)
+	header := make(map[string]int, len(records[0]))
+
+	for i, h := range records[0] {
+		header[h] = i
 	}
+
+	fmt.Println(header)
+
+	for _, r := range records[1:] {
+		fmt.Println(r)
+		fmt.Println(r[header["stop_name"]])
+	}
+}
+
+func initDB(gtfs []string) {
+	_ = os.Remove("data/gtfs_SQL_importer/trippy.db")
+	initCmd := exec.Command("sqlite3", "-init", "data/gtfs_SQL_importer/gtfs_tables.sqlite", "data/trippy.db")
+	initIn, _ := initCmd.StdinPipe()
+
+	if err := initCmd.Start(); err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, f := range gtfs {
+		// Now we want to call a lib written in a real language
+		insertCmd := exec.Command("python", "data/gtfs_SQL_importer/import_gtfs_to_sql.py", f, "nocopy")
+		out, err := insertCmd.Output()
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		_ = insertCmd.Wait()
+
+		if _, err := initIn.Write(out); err != nil {
+			log.Fatalln(err)
+		}
+
+		_, _ = initIn.Write([]byte("\n"))
+	}
+
+	_ = initIn.Close()
+	_ = initCmd.Wait()
 }
 
 func main() {
@@ -37,7 +78,7 @@ func main() {
 	distance := C.distance_between(1.0, 1.0, 2.0, 2.0)
 	fmt.Printf("Go has result, distance is: %v\n", distance)
 
-	initDB()
+	initDB([]string{"data/madrid_emt", "data/madrid_bus"})
 
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
