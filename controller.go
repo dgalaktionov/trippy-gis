f0ca78333179714c42f877cb9f10e8c56cab2416
dagal <galaktionov@gmail.com>
2019-03-26 12:34:23 +0100

    Allow for browser cache

--- controller.go
+++ controller.go
@@ -8,10 +8,12 @@ import (
 func returnGeoJSON(c *gin.Context, fc *geojson.FeatureCollection) {
 	rawJSON, err := fc.MarshalJSON()
 	LogAndPanic2(err, c)
+	c.Header("Cache-Control: max-age", "86400")
 	c.Data(200, "application/json; charset=utf-8", rawJSON)
 }
 
 func Ping(c *gin.Context) {
+	c.Header("Cache-Control: max-age", "86400")
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
