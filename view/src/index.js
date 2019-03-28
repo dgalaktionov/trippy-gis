89ec080f6452f4f6f3d51ec07ecacf537d0c097a
dagal <galaktionov@gmail.com>
2019-03-28 13:20:48 +0100

    Display stop names

--- view/src/index.js
+++ view/src/index.js
@@ -1,9 +1,9 @@
-var css = require ("./index.css");
-var L = require("leaflet");
+let css = require ("./index.css");
+let L = require("leaflet");
 
 function simplyGet(url) {
     return new Promise(function(resolve, reject) {
-        var req = new XMLHttpRequest();
+        let req = new XMLHttpRequest();
         req.open('GET', url);
 
         req.onload = function() {
@@ -23,20 +23,19 @@ function simplyGet(url) {
 }
 
 
-var map = L.map('map', {
+let map = L.map('map', {
     preferCanvas: true
-}).setView([51.505, -0.09], 13);
-var canvasRenderer = L.canvas({ padding: 0.5 });
+}).setView([40.42, -3.70], 13);
+let canvasRenderer = L.canvas({ padding: 0.2 });
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
-
-var stopLayer = L.geoJSON(null, {
+let stopLayer = L.geoJSON(null, {
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup(feature.properties.name);
+    },
     pointToLayer: function(feature, coords) {
         return L.circleMarker(coords, {
             radius: 3,
